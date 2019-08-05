582268a3c0b83091f4584fdc98e531df1734de92
Johnny <johnnycalderondeveloper@gmail.com>
2019-08-05 10:20:24 +0200

    error messages

--- process.php
+++ process.php
@@ -14,11 +14,25 @@ if ($_SERVER["REQUEST_METHOD"] === "POST"){
     } else {
         $name = test_input($_POST['fullname']);
     // check if name only contains letters and whitespace
-    if(!preg_match("/^[a-zA-Z]*$/", $name)){
+    if(!preg_match("/^[a-zA-Z ]*$/", $name)){
         $name_error = "Only letters and whitspace allowed";
       }
     }
+    if (empty($_POST["email"])){
+        $email_error = "Email is required";
+    } else{
+        $email = test_input($_POST["email"]);
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $email_error = "Invalid email format";
+        }
+    }
+  if (empty($_POST["biography"])){
+      $biography_error = "Message is required";
+  } else{
+      $biography = test_input($_POST['biography']);
+  }
 }
+
 
 
 function test_input($data) {
