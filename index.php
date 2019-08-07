49f65cba78a38b00a8857ef612f94ab65f48dcd1
Johnny <johnnycalderondeveloper@gmail.com>
2019-08-07 11:25:26 +0200

    .gitignore

--- index.php
+++ index.php
@@ -6,38 +6,37 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <script src="jquery-3.4.1.min.js"></script>
-    
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="style.css">
-   
 </head>
 <body>
 
 
     <?php include('process.php')?>
     <div class="row justify-content-center">
-    <form action="<?= $SERVER_['PHP_SELF'];?>" method ="POST">
-      <div class ="form group">
-         <label for="your fullname"><strong> Fullname: </strong></strong></label>
-         <input type="text" name="fullname" class="form-control" placeholder = "Your Fullname " value="<?=$name?>">
-         <span class="error"><?= $name_error?></span>
-    </div> 
+    <form action="<?=$_SERVER['PHP_SELF']?>" method ="POST">
+        <div class ="form group">
+            <label for="your fullname"><strong> Fullname: </strong></strong></label>
+            <input type="text" name="fullname" class="form-control" placeholder = "Your Fullname " value="<?=$name?>">
+            <span class="error"><?= $name_error?></span>
+        </div> 
     <br>  
-    <div class ="form group">
-         <label for="Email"><strong>Email:</strong></label>
-         <input type="text" name="email" class="form-control" placeholder = "Email " value="<?=$email?>">
-         <span class="error"><?= $email_error?></span>
+        <div class ="form group">
+        <label for="Email"><strong>Email:</strong></label>
+        <input type="text" name="email" class="form-control" placeholder = "Email " value="<?=$email?>">
+        <span class="error"><?= $email_error?></span>
     </div> 
     <div class ="form group">  
-         <label for="Your message"><strong>Your message</strong> </label>
-         <textarea name="meaasge" class="form-control" value="<?=$message?>"></textarea>
-         <span class="error"><?= $biography_error?></span>
-     </div>  
-     <br>
-     <div class="form group">
+        <label for="Your message"><strong>Your message</strong> </label>
+        <textarea name="message" class="form-control" value="<?=$message?>"></textarea>
+        <span class="error"><?= $message_error?></span>
+    </div>  
+    <br>
+    <div class="form group">
 
 <button type='submit' class="btn btn-primary" ><strong>Submit</strong></button>
 </div>  
--- process.php
+++ process.php
@@ -1,4 +1,7 @@
 <?php 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'vendor/autoload.php';
 
 //----------Define variables--------//
 
@@ -9,14 +12,14 @@ $name = $email = $message = $success = "";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST"){
     if(empty($_POST['fullname'])){
-        $name_error ="Name is required";
+    $name_error ="Name is required";
         
     } else {
         $name = test_input($_POST['fullname']);
     // check if name only contains letters and whitespace
     if(!preg_match("/^[a-zA-Z ]*$/", $name)){
         $name_error = "Only letters and whitspace allowed";
-      }
+    }
     }
     if (empty($_POST["email"])){
         $email_error = "Email is required";
@@ -26,25 +29,59 @@ if ($_SERVER["REQUEST_METHOD"] === "POST"){
             $email_error = "Invalid email format";
         }
     }
-  if (empty($_POST["message"])){
-      $message = "";
-  } else{
-      $message = test_input($_POST['message']);
-  }
-  if ($name_error == "" and $email_error == ""){
-      $message_body = "";
-      unset($_POST['submit']);
-      foreach($_POST as $key => $value){
-          $message_body .= "$key: $value\n";
-      }
-      $to = 'johnnycalderondeveloper@gmail.com';
-      $subject = "Contact Form Submit";
-      if(mail($to, $subject, $message)){
-          $success = "Message sent, thank you";
-          $name = $email = $message = "";
-      }
-  }
+    if (empty($_POST["message"])){
+        $message = "";
+    } else{
+        $message = test_input($_POST['message']);
+    }
+    if ($name_error == "" and $email_error == ""){
+        $message_body = "";
+        unset($_POST['submit']);
+        foreach($_POST as $key => $value){
+        $message_body .= "$key: $value\n";
+        }
+      // Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    include("password.php");
+    $mail->isSMTP();
+    $mail->Host       = "smtp.gmail.com";// Specify main and backup SMTP servers
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = $useremail;                     // SMTP username
+    $mail->Password   = $password;                               // SMTP password
+    $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+    );
+    //Recipients
+    $mail->setFrom($email, $name);
+    $mail->addAddress('johnnycalderondeveloper@gmail.com', 'Joe User');     // Add a recipient
+    $mail->addReplyTo($email);
+    
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = $message_body;
+
+    $mail->send();
+    $success = "Message sent";
+    
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
+    
+    }
+    
+}
+
 
 
 
@@ -53,5 +90,5 @@ function test_input($data) {
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     return $data;
-   }
+}
 ?>
